c02d372e4f4ff4fdf222be3b62cb0e94635cc3e5
Pau Salvador <pasallla@upv.es>
2022-05-08 23:59:21 +0200

    clase1_1

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <a href="https://nextjs.org">devtter</a>
+          <a href="https://nextjs.org">devter</a>
         </h1>
         <nav>
           <Link href='/timeline'>
